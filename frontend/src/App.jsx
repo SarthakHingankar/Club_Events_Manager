dabb298a894a8f685d13d2d111f1f313db8bb8e9
Faizan <faizanfirdousi.official@gmail.com>
2025-02-13 00:13:22 +0530

    setup firebase and signup in components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
 import "./index.css";
-import Landing from './pages/landing';
-
+import Landing from "./pages/landing";
+import Signup from "./components/signup";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Landing />
-    
+      <ToastContainer />
+      <Signup />
     </>
   );
 }
 
-export default App
+export default App;
