f742dc60279c15d22c2416c42cfb716363e64727
Sarthak <sarthakhingankar@gmail.com>
2025-02-13 21:21:55 +0530

    fixed bugs with club api

--- backend/src/controllers/clubController.js
+++ backend/src/controllers/clubController.js
@@ -13,7 +13,7 @@ exports.getAllClubs = async (req, res) => {
 exports.createClub = async (req, res) => {
   try {
     const { name, description } = req.body;
-    const adminId = req.user.uid || 2;
+    const adminId = 2;
 
     const query =
       "INSERT INTO clubs (name, description, created_by) VALUES (?, ?, ?)";
@@ -45,7 +45,7 @@ exports.updateClub = async (req, res) => {
     const { name, description } = req.body;
 
     const [result] = await db.execute(
-      "UPDATE clubs SET name = ?, description = ? WHERE id = ?",
+      "UPDATE clubs SET name = ?, description = ? WHERE club_id = ?",
       [name, description, id]
     );
 
@@ -64,7 +64,7 @@ exports.deleteClub = async (req, res) => {
   try {
     const { id } = req.params;
     await db.execute(`DELETE FROM clubs WHERE club_id = ${id}`);
-    res.status(200);
+    res.status(200).json({ message: "Club deleted successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch clubs" });
