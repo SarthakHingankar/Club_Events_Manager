99816fc2ebe7bfc86dca2aa343b64478dae43dca
Sarthak <sarthakhingankar@gmail.com>
2025-02-13 23:08:31 +0530

    Working on members api

---
+++ backend/src/routes/membersRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+// const { verifyToken, checkRole } = require("../middleware/authMiddleware");
+const {
+  listMembers,
+  joinClub,
+  updateMember,
+  removeMember,
+} = require("../controllers/membersController");
+
+const router = express.Router();
+
+// router.post("/", verifyToken, checkRole("admin"), createClub);
+// router.get("/", verifyToken, getAllClubs);
+
+router.get("/:club_id", listMembers);
+router.post("/:club_id", joinClub);
+router.delete("/:club_id/:user_id", removeMember);
+
+module.exports = router;
